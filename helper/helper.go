4834c9ca5e96d77485d91bab9a37726ab4a3221f
muhtaromzain <muhtaromzain@gmail.com>
2023-11-23 17:55:49 +0700

    access modifier

--- helper/helper.go
+++ helper/helper.go
@@ -1,5 +1,12 @@
 package helper
 
+var version = "1.2.0"
+var Application = "Go"
+
 func SayHello(name string) (string) {
 	return "Hello " + name
+}
+
+func sayGoodBye(name string) (string) {
+	return "Good by " + name
 }
--- import.go
+++ import.go
@@ -7,5 +7,9 @@ import (
 
 func main() {
 	result := helper.SayHello("Zain")
-	fmt.Println(result)
+	fmt.Println(result)\
+
+	fmt.Println(helper.Application) // public
+	fmt.Println(helper.version) // private
+	fmt.Println(helper.sayGoodBye) // private
 }
